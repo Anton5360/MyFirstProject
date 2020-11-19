22e107417d65e5c4159c450af43cb98580f68c42
Anton5360 <a0508520499@gmail.com>
2020-11-19 16:44:20 +0200

    Added directory movement

--- index.php
+++ index.php
@@ -1,60 +1,58 @@
 <?php
+error_reporting(E_ALL);
+$baseDir = __DIR__;
 
-//PHP_EOL
+$scan = scandir(__DIR__);
+$filter = array_filter($scan, function ($value) {
+        return $value[0] !== '.' && strpos($value,'docker') === false;
+});
+$printHtml = '';
+/**
+ * @param array $filt
+ * @param string $curDir
+ * @param string $rout
+ * @return string
+ */
 
-/*$menu = [
-    'title' => 'Samples',
-    'children' => [
-    [
-        'url' => '/',
-        'title' => 'Home',
-    ],
-    [
-        'url' => '/samples/arrays.php',
-        'title' => 'Arrays',
-    ],
-    [
-        'url' => '/samples/basics.php',
-        'title' => 'Basics',
-    ],
-    [
-        'url' => '/samples/branching.php',
-        'title' => 'Branching',
-    ],
-        ]
-];*/
+function dirRun(array $filt, string $curDir = '', string $rout = __DIR__) : string
+{
+    foreach ($filt as $dir) {
+        $rout .= '/' . $dir;
+        $webRout = substr($rout, 9);
+        if(is_dir($rout)){
+            $nestedDir = scandir($rout);
+            $filterNestedDir = array_filter($nestedDir, static function ($value){
+                return $value[0] !== '.';
+            });
+            $curDir = dirRun($filterNestedDir, $curDir, $rout);
+            $rout = dirname($rout);
+        } else {
+            $rout = dirname($rout);
+            $curDir .= "<li><a href='{$webRout}'>{$webRout}</a></li><br>";
+        }
+    }
+    return $curDir;
+}
 
-$taskManager = [
-    [
-        'taskID' => '1',
-        'taskTitle' => 'Work',
-        'taskDescription' => '',
-        'taskOwner' => [
-            'name' => 'Anthony',
-            'surname' => 'Jackenson',
-            'age' => '25'
-        ],
-        'taskDeadline' => [
-            'day' => '25',
-            'month' => 'January',
-            'year' => '2020',
-        ],
-        'taskStatus' => 'In processing'
-    ],
-    [
-        'taskID' => '2',
-        'taskTitle' => 'Workout',
-        'taskDescription' => '',
-        'taskOwner' => [
-            'name' => 'Jack',
-            'surname' => 'Kelson',
-            'age' => '20'
-        ],
-        'taskDeadline' => [
-            'day' => '30',
-            'month' => 'June',
-            'year' => '2021',
-        ],
-        'taskStatus' => 'done',
-    ]
-];
+$printHtml = dirRun($filter);
+
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?= $printHtml ?>
+    </ul>
+</body>
+</html>
